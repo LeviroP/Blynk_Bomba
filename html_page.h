7b830af65c4617c176bf165d5a4126a0dc30f46a
unknown <leviro@gamil.com>
2024-02-18 16:58:19 -0300

    Inclusão tela configurações

--- html_page.h
+++ html_page.h
@@ -56,7 +56,8 @@ R"=====(
      <dt>Station MAC: {MAC}</dt>
    </dl>
    <br><br>
-   <p>Clique aqui para <a href='/info'>Informações do Dispositivo</a>.</p>     
+   <p><a href='/info'>Sobre</a></p>     
+   <p><a href='/param'>Configurar</a></p>     
 <!-----------------------------JavaScript--------------------------->
   <script>
      InitWebSocket()
@@ -134,71 +135,162 @@ R"=====(
 
 
 //=====================================================
+// Webpage Reset: Página exibindo reset em 10s
+//=====================================================
+const char webpageReset[] PROGMEM =
+R"=====(
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reinicialização</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            background-color: #f4f4f4;
+            text-align: center;
+        }
+        .message {
+            font-size: 20px;
+        }
+    </style>
+</head>
+<body>
+<div class="message" id="message">
+    Aguarde, seu dispositivo será reinicializado em <span id="countdown">10</span> segundos.
+</div>
+<script>
+    var count = 10;
+    var countdownElement = document.getElementById("countdown");
+    var messageElement = document.getElementById("message");
+
+    var interval = setInterval(function() {
+        countdownElement.innerText = count;
+        count--;
+
+        if (count < 0) {
+            clearInterval(interval);
+            messageElement.innerText = "Reinicialização realizada!";
+        }
+    }, 1000);
+</script>
+</body>
+</html>
+)=====";
+
+
+//=====================================================
 // Webpage Parametros: Portal Configuração dos Parâmetros
 //=====================================================
 const char webpageParam[] PROGMEM =
 R"=====(
-<!doctype html>
-<html lang='pt-BR'>
-    <head>
-        <meta charset='utf-8'>
-        <meta name='viewport' content='width=device-width, initial-scale=1'>
-        <title>ESP Parâmetros</title>
-        <style>
-            *,
-            ::after,
-            ::before {box-sizing:border-box;}
-            body {margin:0; font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans'; font-size:1rem; font-weight:400; line-height:1.5; color:#212529; background-color:#f5f5f5;}
-            .form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;color: blueviolet;}
-            button{cursor: pointer;border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}
-            .form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}
-            h1{text-align: center;}
-        </style>
-        <script>
-            function c(l) {
-                document.getElementById('1').value='';
-                document.getElementById('2').value='';
-                document.getElementById('3').value='';
-                document.getElementById('4').value='';
-                document.getElementById('5').value='';
-                document.getElementById('6').value='';
-                document.getElementById('7').value='';
-                document.getElementById('8').value='';
-            }
-        </script>
-    </head>
-    <body>
-        <main class='form-signin'>
-            <h1>Configurar Parâmetros</h1>
-            <form action='/param' method='post'>
-                <div class='form-floating'>
-                        <label>Broker MQTT</label>
-                        <input type='text' class='form-control' id='1' name='url' value='{url}'>
-                        <label>Porta Broker</label>
-                        <input type='text' class='form-control' id='2' name='port' value='{port}'>
-                        <label>Usuário</label>
-                        <input type='text' class='form-control' id='3' name='user' value='{user}'>
-                        <label>Senha</label>
-                        <input type='text' class='form-control' id='4' name='user' value='{user}'>
-                        <label>Tópico Publish</label>
-                        <input type='text' class='form-control' id='5' name='pub' value='{pub}'>
-                        <label>Tópico Subscribe</label>
-                        <input type='text' class='form-control' id='6' name='sub' value='{sub}'>
-                        <label>Host mDNS</label>
-                        <input type='text' class='form-control' id='7' name='host' value='{host}'>
-                        <label>Dispositivo Alexa</label>
-                        <input type='text' class='form-control' id='8' name='alexa01' value='{alexa01}'>
-                </div>
-                <br/>
-                <button type='submit'>Salvar</button><p></p>
-                <button type='reset'>Restaurar</button><p></p>
-                <button type='button' onclick='c(this)'>Limpar</button><p></p>
-            </form>
-            <form action='/' method='get'>
-                <button type='botton'>Voltar</button><p></p>
-            </form><br/>
-        </main>
-    </body>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ESP-Parametros</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            padding: 20px;
+            background-color: #f4f4f4;
+        }
+        .form-group {
+            margin-bottom: 15px;
+        }
+        label {
+            display: block;
+            margin-bottom: 5px;
+        }
+        input[type="text"], 
+        input[type="submit"], 
+        button {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ddd;
+            border-radius: 4px;
+            box-sizing: border-box;
+            margin-bottom: 10px;
+        }
+        button {
+            background-color: #f44336;
+            color: white;
+            border: none;
+        }
+        input[type="submit"] {
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+        }
+        h2{text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+<h2>Configurações</h2>
+
+<form action="/param" method="post">
+    <div class="form-group">
+        <label for='url'>Broker MQTT</label>
+        <input type='text' id='url' name='url' placeholder='Broker' value='{url}'>
+    </div>
+    <div class="form-group">
+        <label for='port'>Porta Broker</label>
+        <input type='text' id='port' name='port' placeholder='Porta' value='{port}'>
+    </div>
+    <div class="form-group">
+        <label for='user'>Usuário</label>
+        <input type='text' id='user' name='user' placeholder='Usuário' value='{user}'>
+    </div>
+    <div class="form-group">
+        <label for='pass'>Senha</label>
+        <input type='text' id='pass' name='pass' placeholder='Senha' value='{pass}'>
+    </div>
+    <div class="form-group">
+        <label for='pub'>Tópico Publish/Subscribe - Bomba</label>
+        <input type='text' id='pub' name='pub' placeholder='Publish' value='{pub}'>
+    </div>
+    <div class="form-group">
+        <label for='sub1'>Tópico Subscribe - Sensor0</label>
+        <input type='text' id='sub1' name='sub1' placeholder='Subscribe Sensor0' value='{sub1}'>
+    </div>
+    <div class="form-group">
+        <label for='sub2'>Tópico Subscribe - Sensor1</label>
+        <input type='text' id='sub2' name='sub2' placeholder='Subscribe Sensor1' value='{sub2}'>
+    </div>
+    <div class="form-group">
+        <label for='host'>Host mDNS</label>
+        <input type='text' id='host' name='host' placeholder='Host' value='{host}'>
+    </div>
+    <div class="form-group">
+        <label for='alexa01'>Dispositivo Alexa</label>
+        <input type='text' id='alexa01' name='alexa01' placeholder='Alexa' value='{alexa01}'>
+    </div>
+    <div class="form-group">
+        <label for='api'>Chave API - Trigger Alexa</label>
+        <input type='text' id='api' name='api' placeholder='Chave' value='{api}'>
+    </div>
+    <div class="form-group">
+        <label for='id1'>id1 Trigger - Nível Alto</label>
+        <input type='text' id='id1' name='id1' placeholder='Trigger1' value='{id1}'>
+    </div>
+
+    <div class="form-group">
+        <label for='id2'>id2 Trigger - Nível Baixo</label>
+        <input type='text' id='id2' name='id2' placeholder='Trigger2' value='{id2}'>
+    </div>
+    <input type="submit" value="Salvar">
+    <button type="button" onclick="location.href='/'">Cancelar</button>
+</form>
+</body>
 </html>
 )=====";
 
@@ -238,10 +330,14 @@ const char HTTP_INFO_broker[]     PROGMEM = "<tr><th>Broker MQTT</th><td>{1}<br/
 const char HTTP_INFO_porta[]      PROGMEM = "<tr><th>Porta Broker</th><td>{1}</td></tr>";
 const char HTTP_INFO_user[]       PROGMEM = "<tr><th>Usuário</th><td>{1}</td></tr>";
 const char HTTP_INFO_pass[]       PROGMEM = "<tr><th>Senha</th><td>{1}</td></tr>";
-const char HTTP_INFO_pub[]        PROGMEM = "<tr><th>Tópico Publish</th><td>{1}</td></tr>";
-const char HTTP_INFO_sub[]        PROGMEM = "<tr><th>Tópico Subscribe</th><td>{1}</td></tr>";
+const char HTTP_INFO_pub[]        PROGMEM = "<tr><th>Publish</th><td>{1}</td></tr>";
+const char HTTP_INFO_sub1[]       PROGMEM = "<tr><th>Sensor0</th><td>{1}</td></tr>";
+const char HTTP_INFO_sub2[]       PROGMEM = "<tr><th>Sensor2</th><td>{1}</td></tr>";
 const char HTTP_INFO_hostmdns[]   PROGMEM = "<tr><th>Host mDNS</th><td>http://{1}.local</td></tr>";
-const char HTTP_INFO_alexa01[]    PROGMEM = "<tr><th>Dispositivo Alexa</th><td>{1}</td></tr></table>";
+const char HTTP_INFO_alexa01[]    PROGMEM = "<tr><th>Alexa</th><td>{1}</td></tr>";
+const char HTTP_INFO_api[]        PROGMEM = "<tr><th>Chave API</th><td>{1}<br/>{2}</td></tr>";
+const char HTTP_INFO_id1[]        PROGMEM = "<tr><th>Trigger 1</th><td>{1}</td></tr>";
+const char HTTP_INFO_id2[]        PROGMEM = "<tr><th>Trigger 2</th><td>{1}</td></tr></table>";
 
 const char HTTP_HEAD_START[]      PROGMEM =
 R"=====(
@@ -252,111 +348,11 @@ R"=====(
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=no'/>
     <title>ESP-Info</title>
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            font-family: 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', 'Liberation Sans';
-            font-size: 16px;
-            background-color: #f5f5f5;
-            color: #212529;
-        }
-
-        .container {
-            width: 100%;
-            max-width: 360px;
-            margin: auto;
-            padding: 20px;
-        }
-
-        h1, h3 {
-            color: blue;
-            font-style: italic;
-            text-align: center;
-        }
-
-        hr {
-            margin-top: 10px;
-            margin-bottom: 10px;
-        }
-
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-
-        th, td {
-            text-align: left;
-            padding: 8px;
-        }
-
-        tr:nth-child(odd) {
-            background-color: #f5f5f5; /* Azul claro para linhas ímpares #CAF0F8 */
-        }
-
-        tr:nth-child(even) {
-            background-color: #FFFFFF; /* Vermelho claro para linhas pares #FFC0CB */
-        }
-
-        .button-container {
-            text-align: center;
-            margin-top: 20px;
-        }
-
-        button {
-            cursor: pointer;
-            border: 1px solid transparent;
-            color: #fff;
-            background-color: #007bff;
-            border-color: #007bff;
-            padding: 10px 20px;
-            font-size: 18px;
-            border-radius: 5px;
-            width: auto;
-        }
-
-        @media (max-width: 480px) {
-            body {
-                font-size: 14px;
-            }
-
-            button {
-                padding: 8px 15px;
-                font-size: 16px;
-            }
-        }
-    </style>
-</head>
+    <style>body {margin: 0; padding: 0; font-family: 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', 'Liberation Sans'; font-size: 16px; background-color: #f5f5f5; color: #212529;} .container {width: 100%; max-width: 360px; margin: auto; padding: 20px;} h1, h3 {color: blue; font-style: italic; text-align: center;} hr {margin-top: 10px; margin-bottom: 10px;} table {width: 100%; border-collapse: collapse;} th, td {text-align: left; padding: 8px;} tr:nth-child(odd) {background-color: #f5f5f5;} tr:nth-child(even) {background-color: #FFFFFF;} .button-container {text-align: center; margin-top: 20px;} button {cursor: pointer; border: 1px solid transparent; color: #fff; background-color: #007bff; border-color: #007bff; padding: 10px 20px; font-size: 18px; border-radius: 5px; width: auto;} @media (max-width: 480px) {body {font-size: 14px;} button {padding: 8px 15px; font-size: 16px;}}</style>
+ </head>
 <body>
     <div class='container'>
         <main class='form-signin'>
             <h1>Informações do Dispositivo</h1>
             <hr>
 )=====";                
-
-const char HTTP_HELP[]            PROGMEM =
- "<br/><h3>Available Pages</h3><hr>"
- "<table class='table'>"
- "<thead><tr><th>Page</th><th>Function</th></tr></thead><tbody>"
- "<tr><td><a href='/'>/</a></td>"
- "<td>Menu page.</td></tr>"
- "<tr><td><a href='/wifi'>/wifi</a></td>"
- "<td>Show WiFi scan results and enter WiFi configuration.(/0wifi noscan)</td></tr>"
- "<tr><td><a href='/wifisave'>/wifisave</a></td>"
- "<td>Save WiFi configuration information and configure device. Needs variables supplied.</td></tr>"
- "<tr><td><a href='/param'>/param</a></td>"
- "<td>Parameter page</td></tr>"
- "<tr><td><a href='/info'>/info</a></td>"
- "<td>Information page</td></tr>"
- "<tr><td><a href='/u'>/u</a></td>"
- "<td>OTA Update</td></tr>"
- "<tr><td><a href='/close'>/close</a></td>"
- "<td>Close the captiveportal popup,configportal will remain active</td></tr>"
- "<tr><td>/exit</td>"
- "<td>Exit Config Portal, configportal will close</td></tr>"
- "<tr><td>/restart</td>"
- "<td>Reboot the device</td></tr>"
- "<tr><td>/erase</td>"
- "<td>Erase WiFi configuration and reboot Device. Device will not reconnect to a network until new WiFi configuration data is entered.</td></tr>"
- "</table>"
- "<p/>Github <a href='https://github.com/tzapu/WiFiManager'>https://github.com/tzapu/WiFiManager</a>.";
